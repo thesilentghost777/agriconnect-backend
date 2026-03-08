6eb8f1dfa3fcc9ef29dea7eb7780ae7e8a2dd2e7
root <root@ghost.ghost>
2026-03-08 22:22:57 +0100

    I believe in God

--- database/seeders/WeatherZonesSeeder.php
+++ database/seeders/WeatherZonesSeeder.php
@@ -10,16 +10,25 @@ class WeatherZonesSeeder extends Seeder
     public function run(): void
     {
         $zones = [
+
+            // CAMEROUN
             ['name' => 'Yaoundé Centre',       'region' => 'Centre',      'country' => 'Cameroun', 'latitude' => 3.8667,  'longitude' => 11.5167],
-            ['name' => 'Douala Littoral',       'region' => 'Littoral',    'country' => 'Cameroun', 'latitude' => 4.0511,  'longitude' => 9.7679],
-            ['name' => 'Bafoussam Ouest',       'region' => 'Ouest',       'country' => 'Cameroun', 'latitude' => 5.4737,  'longitude' => 10.4179],
-            ['name' => 'Garoua Nord',           'region' => 'Nord',        'country' => 'Cameroun', 'latitude' => 9.3017,  'longitude' => 13.3963],
-            ['name' => 'Maroua Extrême-Nord',   'region' => 'Extrême-Nord','country' => 'Cameroun', 'latitude' => 10.5913, 'longitude' => 14.3163],
-            ['name' => 'Ngaoundéré Adamaoua',   'region' => 'Adamaoua',    'country' => 'Cameroun', 'latitude' => 7.3167,  'longitude' => 13.5833],
-            ['name' => 'Bertoua Est',           'region' => 'Est',         'country' => 'Cameroun', 'latitude' => 4.5833,  'longitude' => 13.6833],
-            ['name' => 'Ebolowa Sud',           'region' => 'Sud',         'country' => 'Cameroun', 'latitude' => 2.9167,  'longitude' => 11.15],
-            ['name' => 'Bamenda Nord-Ouest',    'region' => 'Nord-Ouest',  'country' => 'Cameroun', 'latitude' => 5.9597,  'longitude' => 10.1455],
-            ['name' => 'Buea Sud-Ouest',        'region' => 'Sud-Ouest',   'country' => 'Cameroun', 'latitude' => 4.1527,  'longitude' => 9.2403],
+            ['name' => 'Douala Littoral',      'region' => 'Littoral',    'country' => 'Cameroun', 'latitude' => 4.0511,  'longitude' => 9.7679],
+            ['name' => 'Bafoussam Ouest',      'region' => 'Ouest',       'country' => 'Cameroun', 'latitude' => 5.4737,  'longitude' => 10.4179],
+            ['name' => 'Garoua Nord',          'region' => 'Nord',        'country' => 'Cameroun', 'latitude' => 9.3017,  'longitude' => 13.3963],
+            ['name' => 'Maroua Extrême-Nord',  'region' => 'Extrême-Nord','country' => 'Cameroun', 'latitude' => 10.5913, 'longitude' => 14.3163],
+
+            // SENEGAL
+            ['name' => 'Dakar',        'region' => 'Dakar',       'country' => 'Sénégal', 'latitude' => 14.7167, 'longitude' => -17.4677],
+            ['name' => 'Saint-Louis',  'region' => 'Saint-Louis', 'country' => 'Sénégal', 'latitude' => 16.0326, 'longitude' => -16.4818],
+            ['name' => 'Kaolack',      'region' => 'Kaolack',     'country' => 'Sénégal', 'latitude' => 14.1469, 'longitude' => -16.0726],
+            ['name' => 'Tambacounda',  'region' => 'Tambacounda', 'country' => 'Sénégal', 'latitude' => 13.7707, 'longitude' => -13.6673],
+
+            // MAROC
+            ['name' => 'Rabat',       'region' => 'Rabat-Salé-Kénitra', 'country' => 'Maroc', 'latitude' => 34.0209, 'longitude' => -6.8416],
+            ['name' => 'Casablanca',  'region' => 'Casablanca-Settat',  'country' => 'Maroc', 'latitude' => 33.5731, 'longitude' => -7.5898],
+            ['name' => 'Marrakech',   'region' => 'Marrakech-Safi',     'country' => 'Maroc', 'latitude' => 31.6295, 'longitude' => -7.9811],
+            ['name' => 'Fès',         'region' => 'Fès-Meknès',         'country' => 'Maroc', 'latitude' => 34.0331, 'longitude' => -5.0003],
         ];
 
         foreach ($zones as $zone) {
